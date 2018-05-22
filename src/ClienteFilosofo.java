3db2061aed6d247c0582c23996c24587d4edc55c
daviidsilvaa <davidsfernandes2@gmail.com>
2018-05-22 09:22:02 -0300

    Filosofo requesita hashi

--- src/ClienteFilosofo.java
+++ src/ClienteFilosofo.java
@@ -1,10 +1,13 @@
 import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.Map;
-
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
 import properties.Properties;
 
 public class ClienteFilosofo {
@@ -19,9 +22,15 @@ public class ClienteFilosofo {
 				Integer.parseInt(properties.get("serverPort"))
 				).waitInit();
 
-//		new ClienteFilosofo(" ", properties.get("serverNeigh"),
-//				Integer.parseInt(properties.get("serverPort"))
-//				).execute();
+		new Thread(new ServerFilosofo(Integer.parseInt(properties.get("philoPort")))).start();
+
+		try{
+			TimeUnit.SECONDS.sleep(1);
+		}catch(Exception e){}
+
+		new ClienteFilosofo(" ", properties.get("serverNeigh"),
+				Integer.parseInt(properties.get("philoPort"))
+				).execute();
 	}
 
 	public ClienteFilosofo(String client, String server, int port) throws UnknownHostException, IOException {
@@ -49,14 +58,62 @@ public class ClienteFilosofo {
 	public void execute() throws IOException, ClassNotFoundException{
 		Map<String, String> properties = new Properties("../config.properties").get();
 
-		System.out.println("Conectando ao filosofo " + properties.get("serverNeigh"));
+		System.out.println("Conectado ao filosofo " + properties.get("serverNeigh"));
 
-//		while(!true);
+		//while(true) {
+			sleep();
+			think();
+			eat();
+			//			System.out.println("recebido");
+		//}
+	}
 
-//		new ObjectInputStream(new BufferedInputStream(socket.getInputStream()));
+	public void sleep() {
+		Random rand = new Random();
+		try{
+			TimeUnit.MILLISECONDS.sleep(rand.nextInt(50));
+			System.out.println("dormindo");
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
 
-//		System.out.println("recebido");
+	public void think() {
+		Random rand = new Random();
+		try{
+			TimeUnit.MILLISECONDS.sleep(rand.nextInt(50));
+			System.out.println("pensando");
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
 
-		this.socket.close();
+	public void eat() throws IOException {
+		Map<String, String> properties = new Properties("../config.properties").get();
+		
+		boolean hashi1 = this.requestHashi("localhost");
+		boolean hashi2 = this.requestHashi(properties.get("serverNeigh"));
+		
+		System.out.println(hashi1 + " : " + hashi2);
+	}
+	
+	public boolean requestHashi(String ip) throws IOException {
+		ObjectOutputStream out = new ObjectOutputStream(new BufferedOutputStream(this.socket.getOutputStream()));
+		Message msg = new Message("pedindoHashi");
+		out.writeObject(msg);
+		out.flush();
+		
+		ObjectInputStream in = new ObjectInputStream(new BufferedInputStream(socket.getInputStream()));
+		try{
+			msg = (Message) in.readObject();
+		}catch(Exception e){
+			e.printStackTrace();;
+		}
+		
+		if(msg.getValue() == "disponivel") {
+			return true;
+		}
+		
+		return false;
 	}
 }
--- src/ServerFilosofo.java
+++ src/ServerFilosofo.java
@@ -1,27 +1,31 @@
 import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Map;
 
 import properties.Properties;
 
-public class ServerFilosofo {
+public class ServerFilosofo implements Runnable{
 	private int port;
 	private ServerSocket server;
 	private Socket socket;
 	private boolean isStopped;
-	
+
 	private boolean myHashi;
 	private boolean neighHashi;
-	
-	public static void main(String[] args) throws IOException {
-		Map<String, String> properties = new Properties("../config.properties").get();
-		
-		new ServerFilosofo(Integer.parseInt(properties.get("serverPort"))).execute();
+
+	public void run(){
+		System.out.println("Porta " + this.port + " aberta!");
+		try{
+			this.socket = this.server.accept();
+			
+		}catch(Exception e){}
 	}
-	
+
 	public ServerFilosofo(int port) throws IOException {
 		this.port = port;
 		this.server = new ServerSocket(this.port);
@@ -29,16 +33,30 @@ public class ServerFilosofo {
 		this.neighHashi = false;
 		this.isStopped = false;
 	}
-	
+
 	public void execute() throws IOException {
-		System.out.println("Porta " + this.port + " aberta!");
-		
-		this.socket = this.server.accept();
-		
 		while(!this.isStopped) {
-			new ObjectInputStream(new BufferedInputStream(socket.getInputStream()));
+			ObjectInputStream in = new ObjectInputStream(new BufferedInputStream(socket.getInputStream()));
+			Message msg = new Message();
 			
+			try{
+				msg = (Message) in.readObject();
+			}catch(Exception e){
+				e.printStackTrace();;
+			}
 			
+			if(msg.getValue() == "pedindoHashi") {
+				if(this.myHashi == false) {
+					msg.setValue("disponivel");
+				} else {
+					msg.setValue("indisponivel");
+				}
+			}
+			
+			ObjectOutputStream out = new ObjectOutputStream(new BufferedOutputStream(this.socket.getOutputStream()));
+			
+			out.writeObject(msg);
+			out.flush();
 		}
 	}
 }
